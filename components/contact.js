24b389566e518e5ba952e4bb5420a295b9c382e0
sanket mishra <sanketmishra@protonmail.com>
2020-10-12 13:52:57 +0530

    send email

--- components/contact.js
+++ components/contact.js
@@ -1,9 +1,17 @@
 import React,{Component} from 'react';
 import {View,Text} from 'react-native';
-import {Card} from 'react-native-elements';
+import {Card, Button, Icon} from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component{
+    sendMail(){
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        })
+    }
 
     render(){
         const title ="Contact Information";
@@ -21,6 +29,12 @@ class Contact extends Component{
     Fax: +852 8765 4321
     Email:confusion@food.net
                     </Text>
+                    <Button 
+                    title="send email"
+                    buttonStyle={{backgroundColor:'#512DA8'}}
+                    icon={<Icon name="envolope-o" type="font-awesome" color="white" />}
+                    onPress={this.sendMail}
+                    />
                 </Card>
             </Animatable.View>
         );
